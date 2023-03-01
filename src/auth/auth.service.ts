bc4e91c9e003847094aca8359d21aedf0525e656
Wallace <dev_kollen@outlook.com>
2023-03-01 01:46:45 -0300

    feat(auth): implement local strategy authorization

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class AuthService {
-    validateUser(email: string, password: string) {
-        throw new Error('Method not implemented.');
+
+    constructor(private readonly userService: UserService) {}
+
+    async validateUser(email: string, password: string) {
+        const user = await this.userService.findByEmail(email);
+
+        if (user) {
+            
+            const isPasswordValid = await bcrypt.compare(password, user.password);
+
+            if (isPasswordValid) return {...user, password: undefined};
+        }
+
+        throw new UnauthorizedException('Email address or password provided is incorrect.');
     }
     login() {
         throw new Error('Method not implemented.');
