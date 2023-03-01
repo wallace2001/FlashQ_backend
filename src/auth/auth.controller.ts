5c8096969ed845237a019a085e95b7f8de5cc449
Wallace <dev_kollen@outlook.com>
2023-03-01 02:33:16 -0300

    feat(auth): implement jwt validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,20 @@
-import { Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { IsPublic } from './decorators/is-public.decorator';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthRequest } from './models/AuthRequest';
 
 @Controller()
 export class AuthController {
 
     constructor(private readonly authService: AuthService) {}
 
+    @IsPublic()
     @Post("login")
     @HttpCode(HttpStatus.OK)
     @UseGuards(LocalAuthGuard)
-    login() {
-        this.authService.login();
+    login(@Request() req: AuthRequest) {
+        return this.authService.login(req.user);
     }
 
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,25 +1,42 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from "bcrypt";
+import { User } from 'src/user/entities/user.entity';
+import { UserPayload } from './models/UserPayload';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './models/UserToken';
 
 @Injectable()
 export class AuthService {
 
-    constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService, private readonly jwtService: JwtService) { }
+
+    login(user: User): UserToken {
+        const { id, email, name } = user;
+
+        const payload: UserPayload = {
+            sub: id,
+            name,
+            email
+        };
+
+        const jwtToken = this.jwtService.sign(payload);
+
+        return {
+            access_token: jwtToken
+        };
+    }
 
     async validateUser(email: string, password: string) {
         const user = await this.userService.findByEmail(email);
 
         if (user) {
-            
+
             const isPasswordValid = await bcrypt.compare(password, user.password);
 
-            if (isPasswordValid) return {...user, password: undefined};
+            if (isPasswordValid) return { ...user, password: undefined };
         }
 
         throw new UnauthorizedException('Email address or password provided is incorrect.');
-    }
-    login() {
-        throw new Error('Method not implemented.');
     }
 }
